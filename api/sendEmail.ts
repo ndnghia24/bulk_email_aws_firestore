cc13138ac2348f14b1156bcb860a2b3745275060
Your Name <ngoducnghia01648927528@gmail.com>
2024-11-28 22:20:56 +0700

    done

--- api/sendEmail.ts
+++ api/sendEmail.ts
@@ -5,7 +5,7 @@ const AWS = require('aws-sdk');  // Import AWS SDK
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_NDNGHIA24,
   secretAccessKey: process.env.AWS_SECRET_KEY_NDNGHIA24,
-  region: process.env.AWS_REGION || 'us-east-1',  // Nếu không có region trong .env thì mặc định là 'us-east-1'
+  region: process.env.AWS_REGION || 'ap-southeast-2',
 });
 
 const ses = new AWS.SES();  // Tạo instance của SES
