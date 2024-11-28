a302ad01778e2cb97b1da5e56ca47148e24881dc
Your Name <ngoducnghia01648927528@gmail.com>
2024-11-28 20:21:03 +0700

    done

--- api/sendEmail.ts
+++ api/sendEmail.ts
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
     const response = await fetch('https://api.elasticemail.com/v4/emails', {
       method: 'POST',
       body: JSON.stringify({
-        apikey: 'YOUR_ELASTIC_EMAIL_API_KEY',
+        apikey: '1D379C34823C8E62F6001EEC64879FDB3ECBFC6CE8F09F51B6BA441C6001E81E3FAA59498BE3F3FE370BECAD75D478B2',
         from: 'ngoducnghia01648927528@gmail.com',
         to:'ngoducnghia01648927528@gmail.com',
         subject: 'Khuyến mãi hôm nay!',
