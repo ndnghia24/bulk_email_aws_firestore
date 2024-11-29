b0e827c7023d52624d4bb59d48977554431e4653
Your Name <ngoducnghia01648927528@gmail.com>
2024-11-29 22:59:15 +0700

    done

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -21,6 +21,10 @@ const saveSubscribers = async (subscribers) => {
 };
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   switch (req.method) {
     case 'POST':
       const { email, location } = req.body;
